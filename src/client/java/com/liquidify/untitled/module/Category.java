c193f2240f6500de6ed5a36f950313ff983e3499
Liquidify <ps5mv59frx@privaterelay.appleid.com>
2026-03-01 16:08:06 +0300

    Delete src directory
    to update

--- src/client/java/com/liquidify/untitled/module/Category.java
+++
@@ -1,5 +0,0 @@
-package com.liquidify.untitled.module;
-
-public enum Category {
-    COMBAT, MOVEMENT, RENDER, PLAYER
-}
--- src/client/java/com/liquidify/untitled/module/settings/BooleanSetting.java
+++
@@ -1,9 +0,0 @@
-package com.liquidify.untitled.module.settings;
-
-public class BooleanSetting extends Setting<Boolean> {
-    public BooleanSetting(String name, boolean defaultValue) {
-        super(name, defaultValue);
-    }
-
-    public void toggle() { value = !value; }
-}
